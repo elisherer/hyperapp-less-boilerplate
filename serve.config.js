ab518c2ad57482e8eb2c169a6ca42b17d06054af
Eli Sherer <eli.sherer@gmail.com>
2018-12-31 23:44:05 +0200

    Delete serve.config.js

--- serve.config.js
+++
@@ -1,7 +0,0 @@
-const webpackServeWaitpage = require('webpack-serve-waitpage');
-
-module.exports = {
-  add: (app, middleware, options) => {
-    app.use(webpackServeWaitpage(options, { theme: 'material' }));
-  }
-};
